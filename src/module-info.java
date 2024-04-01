f56d522b9ccdc246113bc8ecd462c8e5cd939c5f
Jose <jomaamga@outlook.es>
2024-04-01 10:09:27 +0200

    init commit

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+module pi5 {
+	requires transitive datos_compartidos;
+	requires transitive grafos;
+	requires transitive org.jgrapht.core;
+	requires transitive org.jgrapht.io;
+	requires transitive partecomun;
+	requires transitive solve;
+}
